fbbe938dc72b36f6f153aaef192a0cead4602f74
Khandker <khandker.islam46@gmail.com>
2021-11-15 19:43:00 -0500

    game logic and stuff

--- server/server.js
+++ server/server.js
@@ -1,7 +1,19 @@
 const path = require('path');
 const express = require('express');
+const { createServer } = require('http');
+const { Server } = require("socket.io");
+const uuid = require('uuid');
+const { initGame } = require('./game');
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer); 
+
+//Global Variables for the Server
+let Frame = 0;  // count game frames
+const FramesPerGameStateTransmission = 3; // How game state is broadcasted to players - per three frames
+const MaxConnections = 10; // 10 players?? 
+const Connections = {}; // Contains player state  and websocket info like IP.
 
 // import routers
 
@@ -15,6 +27,77 @@ app.use(express.static(path.resolve(__dirname, '../dist')));
 app.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '../dist/index.html'));
 });
+
+
+const clientRooms = {}; //map client ids to rooms
+
+io.on('connection', (client) => {  
+  // const state = createGameState(); //Create Gamestate as soon as player connects.
+  //figure out key actions here 
+
+  // client.on('keydown', handleKeydown);
+  // client.on('newGame', handleNewGame);
+  // client.on('joinGame', handleJoinGame);
+
+  function handleJoinGame(gameCode){
+    const room = io.sockets.adapter.rooms[gameCode];
+
+    let allUser; 
+    if (room) {
+      allUsers = room.sockets; //object of all the current users in room.  key is client id, object is client itself.
+    }
+
+    let numCliencts = 0; 
+    if (allUsers) {
+      numClients = Object.keys(allUsers).length; //Number of clients 
+    }
+    
+    if (numClients == 0) //no one in this room, game/room doesn't exist
+    { 
+      client.emit('unknownGame');
+      return;
+    }
+    else if (numClients > 1){ //two player game for now...
+      client.emit('tooManyPlayers');
+      return;
+    }
+
+    clientRooms[client.id] = gameCode;
+    client.join(gameCode);
+    client.number = 2; //Second player to join a game; 
+    client.emit('init',2); //Two player game
+  }
+
+  function handleNewGame() { //Create a new room.
+    let roomName = makeId(5); //generate 5 character id
+    clientRooms[client.id] = roomName;
+    client.emit('gameCode', roomName);
+
+    state[roomName] = initGame(); //create room as a part of state in global state object.
+    client.join(roomName);  //add client to room
+    client.number = 1; //set current client to player 1, because they created the room.
+    client.emit('init', 1); //send this back to frontend so its aware the number of the current player.
+  }
+
+  function handleKeydown() { 
+    //handle user actions here...
+  }
+});
+
+function startGameInterval(roomName) {
+
+}
+
+function emitGameState(gameState) {
+  // Send this event to everyone in the room.
+  io.sockets.in(room)
+    .emit('gameState', JSON.stringify(gameState));
+}
+
+function emitGameOver(room, winner) {
+  io.sockets.in(room)
+    .emit('gameOver', JSON.stringify({ winner }));
+}
 
 // 404 handler
 app.use((req, res) => res.status(404).send('Page not found!'));
@@ -33,6 +116,11 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`server listening on port ${PORT}`);
-});
+
+
+//app.listen will create a new http server, so we don't use that i guess.
+// app.listen(PORT, () => { 
+//   console.log(`server listening on port ${PORT}`);
+// });
+
+server.listen(3000, () => {  console.log('listening on *:3000');});
